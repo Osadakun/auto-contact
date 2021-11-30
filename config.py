165fd6aead8d38ee2ed7ffd0b08670db5af39719
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-11-30 20:03:41 +0900

    the-first

--- config.py
+++ config.py
@@ -1,14 +1,4 @@
 #-*- cording: utf-8 -*-
-from linebot.models import *
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-import function
-import config
-import psycopg2
-from flask import *
-import json
+DB_URL = "postgres://ufdamuwhxvmuop:c0b17ed71fa8f8f74a357d70bd9a556aa344035f671936edd0a2a4c287779cb4@ec2-54-198-213-75.compute-1.amazonaws.com:5432/ddisvrngf39un4"
+CHANNEL_SECRET = "a2b6aac51423c9c5c3b8b968366d8f01"
+ACCESS_TOKEN = "/HcSvopbB4B5/VhCmzLPBE3RP5PGW9ymaqqdNBzS/5JZqwdV+ZnxSz3jdtJ9aIRCBmLMWNFMJBjwNF8ZydomBRQD1OmxIDZQpF6XCY3pIEb6zckXv08sANpEBKfR2FCSUFbRgcPptsnds6WNITY88gdB04t89/1O/w1cDnyilFU="
--- main.py
+++ main.py
@@ -9,3 +9,56 @@ from linebot.exceptions import (
 )
 import function
 import config
+
+app = Flask(__name__)
+line_bot_api = LineBotApi(config.ACCESS_TOKEN)
+handler = WebhookHandler(config.CHANNEL_SECRET)
+
+def hello_world():
+    return "HelloWorld!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+	"""
+	友だち追加したときのイベント。
+	UsersDBにID、アクティビティを追加。
+	"""
+	UserID = event.source.user_id
+	line_bot_api.reply_message(event.reply_token,
+		[
+			TextSendMessage(text='minatoJBSC【公式】です。\n友達追加ありがとうございます!!'),
+			TextSendMessage(text='これからは練習の休み、遅刻の連絡はこのアカウントからお願いします。'),
+			TextSendMessage(text='本日の練習をお休みする場合は「休み」、遅れて参加の場合は「遅刻」と送信して下さい。')
+		]
+	)
+
+	UserID = event.source.user_id
+	BadPush.add(UserID)
+	line_bot_api.reply_message(event.reply_token,
+		[
+			TextSendMessage(text='minatoJBSC【公式】です。\n友達追加ありがとうございます!!'),
+			TextSendMessage(text='これからは練習の休み、遅刻の連絡はこのアカウントからお願いします。'),
+			TextSendMessage(text='本日の練習をお休みする場合は「休み」、遅れて参加の場合は「遅刻」と送信して下さい。')
+		]
+	)
+
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+	"""
+	ブロックされた時のイベント。
+	UsersDBのIDとアクティビティを削除。
+	"""
+	UserID = event.source.user_id
+	BadPush.remove(UserID)
