efcb83073b5d48b0870c305f75411b0838f3aad8
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-12-01 23:04:57 +0900

    debug

--- function.py
+++ function.py
@@ -1,9 +1,12 @@
 import psycopg2
 
 def SQL_add(URL,id):
+    print(id)
     id = str(id)
+    print(id)
+    print("%s" %id)
     con = psycopg2.connect(URL, sslmode='require')
     cursor = con.cursor()
-    cursor.execute("INSERT INTO Informations(userid,status,reason_time,name,remarks) values (%s,'なし','なし','なし','なし');" %(id))
+    # cursor.execute("INSERT INTO Informations(userid,status,reason_time,name,remarks) values (%s,'なし','なし','なし','なし');" %(id))
     con.commit()
     print("--------------------------")
