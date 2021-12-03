5d953f1108eccc556377be85c465cb26e6880988
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-12-03 15:22:46 +0900

    update by two person

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #-*- cording: utf-8 -*-
 import os
-DB_URL = os.environ(DATABASE_URL)
-CHANNEL_SECRET = os.environ(CHANNEL_SECRET)
-ACCESS_TOKEN = os.environ(ACCESS_TOKEN)
+DB_URL = os.environ[DATABASE_URL]
+CHANNEL_SECRET = os.environ[CHANNEL_SECRET]
+ACCESS_TOKEN = os.environ[ACCESS_TOKEN]
